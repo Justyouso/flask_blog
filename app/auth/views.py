f8ef513f317849847e924daecdd8979875736005
justyouso <1242646507@qq.com>
2019-06-24 16:11:58 +0800

    fix: 修改已知bug

--- app/auth/views.py
+++ app/auth/views.py
@@ -20,7 +20,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return redirect(request.args.get("next") or url_for('main.index'))
+            # return redirect(request.args.get("next") or url_for('main.index'))
+            return redirect(url_for('main.index'))
         flash("invalid username or password")
     return render_template('auth/login.html', form=form)
 
@@ -68,7 +69,7 @@ def before_request():
     if current_user.is_authenticated:
         # 写入登录时间
         current_user.ping()
-        if not current_user.confirmed and request.endpoint[:5]!=auth:
+        if not current_user.confirmed and request.endpoint[:5] != "auth.":
             return redirect(url_for('auth.unconfirmed'))
 
 
