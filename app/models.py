a828845a7a447799984a66e0c9279e91fd47afe3
justyouso <1242646507@qq.com>
2019-12-04 14:29:48 +0800

    fix: 修改model

--- app/models.py
+++ app/models.py
@@ -289,6 +289,7 @@ login_manager.anonymous_user = AnonymousUser
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True,
                           default=datetime.utcnow)
